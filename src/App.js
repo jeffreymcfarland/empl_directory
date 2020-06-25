76fcdb9f04b1d1b0cfe6d92ff61e88b9457c18ce
Jeffrey McFarland <jlmcfarlandj@gmail.com>
2020-06-24 22:59:32 -0500

    implement randomAPI axios call and API component, console log data res

--- src/App.js
+++ src/App.js
@@ -1,14 +1,27 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import Header from './components/Header';
-import Searchbar from './components/Searchbar'
+import Searchbar from './components/Searchbar';
+import API from "./utils/API";
 
 function App() {
   const [search, setSearch] = useState("");
 
+  useEffect(() =>{
+    API.getRandomEmployees()
+    .then(res => {
+      console.log(res.data);
+    });
+  },[])
+
   const handleSubmit = e => {
     e.preventDefault();
+    searchEmployees();
     console.log(search);
   };
+
+  const searchEmployees = () => {
+    
+  }
 
   return (
     
